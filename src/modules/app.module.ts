2d705b2e5b96e92280b2d6d5130e9a2ecf526587
yangjiedong <yangjiedong@szzbmy.com>
2017-12-24 22:00:43 +0800

    A Ares Demo

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import {Module, NestModule, MiddlewaresConsumer} from '@nestjs/common';
+import {LoggerMiddleware} from './common/middlewares/logger.middleware';
+import {CatsModule} from './cats/cats.module';
+import {CatsController} from './cats/cats.controller';
 
 @Module({
-  modules: [],
-  controllers: [AppController],
-  components: [],
+    modules: [CatsModule],
 })
-export class ApplicationModule {}
+export class ApplicationModule implements NestModule {
+    
+    configure(consumer: MiddlewaresConsumer): void {
+        consumer
+            .apply(LoggerMiddleware)
+            .with('ApplicationModule')
+            .forRoutes(CatsController);
+        // return super.configure(consumer);
+    }
+}
