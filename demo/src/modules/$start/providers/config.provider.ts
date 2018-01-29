1dd9cf2c0ffdafe0dbc8c30d69e195fbb67a2433
yangjiedong <yangjiedong@szzbmy.com>
2018-01-29 23:07:15 +0800

    D ignore

--- demo/src/modules/$start/providers/config.provider.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-
-const configPath = path.resolve(require.main.filename, '..', 'config.js');
-const config = require(configPath);
-
-export const ConfigProvider = {
-    provide: 'config',
-    useValue: config
-};
